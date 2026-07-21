ad182f34a6230534f408f58e81d55db8f5cc8037
Tesoley <01dashakobylianska@gmail.com>
2026-07-21 16:10:59 +0300

    The 2nd task is completed: regional_tariffs.json and global_sales.csv files were read successfully

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import json
 import matplotlib.pyplot as plt
 import pandas as pd
 
+with open('regional_tariffs.json', 'r', encoding = 'utf-8') as file:
+    regional_tariffs = json.load(file)
+
+with open('global_sales.csv', 'r', encoding = 'utf-8') as file:
+    reader = csv.DictReader(file)
+    sales_data = list(reader)
+
+    # successfully
